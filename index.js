9c54a406de553d659461a652d60964ab407976e0
Juan Carlos Patron <charliepatronr@gmail.com>
2019-12-31 04:45:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+let arr = [];
+function theBeatlesPlay(musicianArr, instArr){
+for (let i = 0 ; i < musicianArr.length; i ++){
+   arr.push(musicianArr[i] + " plays " + instArr[i]);
+}
+return arr;
+}
+
+
+var i  = 0; 
+var newArr =[];
+
+
+function johnLennonFacts(arr){
+   while(i < arr.length){
+      newArr.push(arr[i].concat("!!!"));
+      i++;
+   }
+return newArr;
+}
+
